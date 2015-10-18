2ad59d9d84b0d3bc41d6425aef289511a62bc40c
Justin Ith <jith@u.washington.edu>
2015-10-18 07:52:09 -0700

    Created picture upload

--- views/hunt/js/itemList.js
+++ views/hunt/js/itemList.js
@@ -1,7 +1,56 @@
 (function() {
 
+    var fakeInput = {
+        items:[
+            {id:"0001",
+             name:"Water Bottle",
+             points:20,
+             earned:false
+             },
+             {id:"0002",
+             name:"Umbrella",
+             points:10,
+             earned:true
+             },
+             {id:"0003",
+             name:"Wallet",
+             points:10,
+             earned:false
+             },
+             {id:"0004",
+             name:"Kitten",
+             points:30,
+             earned:true
+             },
+             {id:"0005",
+             name:"Husky",
+             points:30,
+             earned:false
+             }
+        ]
+    }
+
+    var fakeItems = {
+        '0001':{
+            img:'https://upload.wikimedia.org/wikipedia/commons/0/07/Multi-use_water_bottle.JPG'
+        },
+        '0002':{
+            img:'http://ak.picdn.net/shutterstock/videos/2624345/preview/stock-footage-umbrella-on-the-road-under-rain.jpg'
+        },
+        '0003':{
+            img:'https://s-media-cache-ak0.pinimg.com/736x/43/bf/a4/43bfa44e28e6badab50c614cf72004b5.jpg'
+        },
+        '0004':{
+            img:'http://images4.fanpop.com/image/photos/16100000/Cute-Kitten-kittens-16123796-1280-800.jpg'
+        },
+        '0005':{
+            img:'https://upload.wikimedia.org/wikipedia/commons/d/d2/Siberian_Husky_with_Blue_Eyes.jpg'
+        },
+    }
+
     window.onload = function(){
-    	initListeners();
+        loadItems();
+        initListeners();
     }
 
     function initListeners(){
@@ -14,4 +63,39 @@
     	});
     }
 
+    function loadItems(){
+        var items = fakeInput.items;
+        for(var i = 0; i < items.length; i++){
+            var itemName = items[i].name;
+            var itemPoints = items[i].points;
+            var earned = items[i].earned;
+            if(earned){
+                addItem(itemName,itemPoints,earned,fakeItems[items[i].id].img)
+            } else {
+                addItem(itemName,itemPoints,earned);
+            }
+        }
+    }
+
+    function addItem(name,score,earned,imgLink){
+        var baseDiv;
+        if(earned){
+            baseDiv = '<div class="feedItem" style="background: url(' + "'" + imgLink + "'" + ') no-repeat center center; background-size: cover;">' + '<h2>' + name + '</h2><span class="earned">+' + score + '</span></div>';
+        } else {
+            baseDiv = '<div class="feedItem"><h2>' + name + '</h2><span>+' + score + '</span></div>';
+        }
+        $(".feed").append(baseDiv);
+    }
+
 })();
+
+
+
+
+
+
+
+
+
+
+
--- views/hunt/js/itemprofile.js
+++ views/hunt/js/itemprofile.js
@@ -8,6 +8,29 @@
     	$(".backButton").click(function(){
     		location.href = location.href.replace('/itemprofile.html','') + "/itemList.html";
     	});
+
+    	$("#pictureUpload").change(function(){
+    		alert("Uploaded photo!");
+    		console.log($("#pictureUpload").val());
+    		$(".headerImage").css("background");
+    	});
+
+    	createItemContent("Husky",40,true,'https://upload.wikimedia.org/wikipedia/commons/d/d2/Siberian_Husky_with_Blue_Eyes.jpg','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut ullamcorper orci. Sed pellentesque sapien ut nibh eleifend cursus.');
+    }
+
+    function createItemContent(name,points,earned,imgLink,description){
+    	var baseDiv = '<div class="headerImage"';
+    	if(earned){
+    		baseDiv = baseDiv + 'style="background: url(' + "'" + imgLink + "'" + ') no-repeat center center; background-size: cover;"';
+    	}
+    	baseDiv = baseDiv + '><h2>' + name + '</h2><span';
+    	if(earned){
+    		baseDiv = baseDiv + ' class="earned"';
+    	}
+    	baseDiv = baseDiv + ">+" + points + "</span></div><p>" + description + "</p>";
+    	$(".itemContent").append(baseDiv);
+
+    	$("#topTitle").html(name);
     }
 
 })();
