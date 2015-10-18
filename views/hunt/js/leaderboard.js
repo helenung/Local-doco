731fc0d56b97a2cc773a48b225c34447a2be6edc
Justin Ith <jith@u.washington.edu>
2015-10-18 04:26:56 -0700

    Created leaderboard static

---
+++ views/hunt/js/leaderboard.js
@@ -0,0 +1,24 @@
+(function() {
+
+    window.onload = function(){
+    	initListeners();
+        initRank();
+    }
+
+    function initListeners(){
+        $("#itemListNav").click(function(){
+            location.href = location.href.replace('/leaderboard.html','') + "/index.html";
+        });
+    }
+
+    function initRank () {
+        var rankNum = 1;
+        $(".rank").each(function() {
+            if(rankNum%2==1){
+                $(this).addClass('whiteBg');
+            }
+            rankNum++;
+        });
+    }
+
+})();
