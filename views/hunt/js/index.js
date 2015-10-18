771988f4958432e8117c4fd6ff0b03a208adcaf1
Justin Ith <jith@u.washington.edu>
2015-10-18 14:46:38 -0700

    Linking item list with item profiles

--- views/hunt/js/index.js
+++ views/hunt/js/index.js
@@ -6,36 +6,36 @@
       // Handler for .ready() called.
       $( "#startButton" ).click(function() {
        	var url = window.location.href.split("?");
-        	var params = url && url[1] ? url[1].split("=") : null;
-        	huntId = params && params[1] ? params[1] : null;
+      	var params = url && url[1] ? url[1].split("=") : null;
+      	huntId = params && params[1] ? params[1] : null;
 
-        	namae = $("#name").val();
-        	console.log(huntId+ " "+ namae);
-       		db.child("hunts").child(huntId).child("teams").child(namae).set({
-            name: namae
-          });
+      	namae = $("#name").val();
+      	console.log(huntId+ " "+ namae);
+     		db.child("hunts").child(huntId).child("teams").child(namae).set({
+          name: namae
+        });
 
-       		db.child('hunts').child(huntId).child('items').once('value', function(snapshot) {
-    	      var items = snapshot.val();
-            console.log("Items!" + items);
-            for (var i = 0; i < items.length; i++) {
-                //items.push(snapshot.val());
-                console.log(items[i]);
+     		db.child('hunts').child(huntId).child('items').once('value', function(snapshot) {
+  	      var items = snapshot.val();
+          console.log("Items!" + items);
+          for (var i = 0; i < items.length; i++) {
+              //items.push(snapshot.val());
+              console.log(items[i]);
 
-                db.child("hunts").child(huntId).child("teams").child(namae).child('items').set({
-                 [i]: false
-                });
-                
-            }
-    	    });
+              db.child("hunts").child(huntId).child("teams").child(namae).child('items').set({
+               [i]: false
+              });
+              
+          }
+  	    });
 
-          var newLocation = location.href.split("index.html")[0] + "itemList.html?random=" + huntId + ":qw:" + namae;
+        var newLocation = location.href.split("index.html")[0] + "itemList.html?random=" + huntId + ":qw:" + namae;
 
-          setTimeout(function(){
-            location.href = newLocation;
-          },1000);
+        setTimeout(function(){
+          location.href = newLocation;
+        },1000);
 
-          // location.href = location.href.replace('/index.html','') + "/itemList.html";
+        // location.href = location.href.replace('/index.html','') + "/itemList.html";
 
     	});
     });
