cef572d0557066d7ff603c7e3a8d9ccd0524cbe7
unknown <cederw@uw.edu>
2015-10-18 04:51:17 -0700

    dfsdf

--- views/index.js
+++ views/index.js
@@ -15,7 +15,32 @@
         console.log("Login Failed!", error);
       } else {
         console.log("User data:", authData);
+        var id = authData.uid.substring(9);
+
+        checkIfUserExists(id);
+        
       }
     });
   };
+function userExistsCallback(userId, exists) {
+  if (exists) {
+    alert('user ' + userId + ' exists!');
+  } else {
+    alert('user ' + userId + ' does not exist!');
+    var usersRef = db.child("users");
+        
+        usersRef.child(userId).set({
+          id:userId
+        });
+  }
+}
+
+// Tests to see if /users/<userId> has any data. 
+function checkIfUserExists(userId) {
+  db.child('users').child(userId).once('value', function(snapshot) {
+    var exists = (snapshot.val() !== null);
+    userExistsCallback(userId, exists);
+  });
+}
+
 })();
